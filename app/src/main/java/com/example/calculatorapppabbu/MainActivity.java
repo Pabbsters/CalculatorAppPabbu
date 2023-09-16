750241916f6f02a1190d76fc78ff37c001cb3fa6
Pabbsters <ruthwikpabbu@gmail.com>
2023-09-15 19:46:05 -0500

    got the calculations to work

--- app/src/main/java/com/example/calculatorapppabbu/MainActivity.java
+++ app/src/main/java/com/example/calculatorapppabbu/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.calculatorapppabbu;
 
+import static java.lang.Double.sum;
+
 import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
@@ -17,8 +19,7 @@ import android.widget.EditText;
 public class MainActivity extends AppCompatActivity{ //implements View.OnClickListener{
     public EditText edittxt1, edittxt2;
     public Button addbutton,subbutton,mulbutton, divbutton;
-    public  TextView result;
-    int number1, number2;
+    public TextView textresult;
 
 
     @SuppressLint("MissingInflatedId")
@@ -31,40 +32,83 @@ public class MainActivity extends AppCompatActivity{ //implements View.OnClickLi
 
         edittxt1= (EditText)findViewById(R.id.editText1);
         edittxt2=findViewById(R.id.editText2);
-        addbutton=findViewById(R.id.buttonplus);
-        result=findViewById(R.id.viewResult);
+        addbutton=(Button)findViewById(R.id.buttonplus);
+        subbutton=(Button)findViewById(R.id.buttonminus);
+        mulbutton=(Button)findViewById(R.id.buttonmul);
+        divbutton=(Button)findViewById(R.id.buttondiv);
+        textresult = (TextView) findViewById(R.id.viewResult);
 
-        //addbutton.setOnClickListener(this);
-        //number1=Integer.parseInt(edittxt1.getText().toString()); // has to be inside a function
-    //    number2=Integer.parseInt(edittxt2.getText().toString()); // has to be inside of a function.
 
-
-    }
-
-    public void methodSum(View v){
-        int sum=number1+number2;
-        result.setText(sum);
-        number1=Integer.parseInt(edittxt1.getText().toString());
-        number2=Integer.parseInt(edittxt2.getText().toString());
-    }
-
-    //@Override
-    /*public void onClick(View view) {
-        if(edittxt2.getText().toString().equals(" ")){
-            Toast.makeText(this,"Enter number",Toast.LENGTH_SHORT).show();
-        }
-        number1=Integer.parseInt(edittxt1.getText().toString());
-        number2=Integer.parseInt(edittxt2.getText().toString());
-
-        if(addbutton == view.getId()){
-            result.setText(number1+number2);
-
+    // Performing Addition of 2 numbers
+    addbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+          if((edittxt1.getText().length()>0) && (edittxt2.getText().length()>0)){
+              double num1=Double.parseDouble((edittxt1.getText().toString()));
+              double num2=Double.parseDouble((edittxt2.getText().toString()));
+              double result= num1+num2;
+              textresult.setText(Double.toString(result));
+          }
+          else{
+              Toast toast=Toast.makeText(MainActivity.this," Enter the numbers", Toast.LENGTH_LONG);
+              toast.show();
+          }
 
         }
 
+    });
+
+        // Performing Subtraction of 2 numbers
+        subbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if((edittxt1.getText().length()>0) && (edittxt2.getText().length()>0)){
+                    double num1=Double.parseDouble((edittxt1.getText().toString()));
+                    double num2=Double.parseDouble((edittxt2.getText().toString()));
+                    double result= num1-num2;
+                    textresult.setText(Double.toString(result));
+                }
+                else{
+                    Toast toast=Toast.makeText(MainActivity.this," Enter the numbers", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+            }
+
+        });
+
+        // Performing Multiplication  of 2 numbers
+        mulbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if((edittxt1.getText().length()>0) && (edittxt2.getText().length()>0)){
+                    double num1=Double.parseDouble((edittxt1.getText().toString()));
+                    double num2=Double.parseDouble((edittxt2.getText().toString()));
+                    double result= num1*num2;
+                    textresult.setText(Double.toString(result));
+                }
+                else{
+                    Toast toast=Toast.makeText(MainActivity.this," Enter the numbers", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+            }
+
+        });
+        // Performing Division of 2 numbers
+        divbutton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                if((edittxt1.getText().length()>0) && (edittxt2.getText().length()>0)){
+                    double num1=Double.parseDouble((edittxt1.getText().toString()));
+                    double num2=Double.parseDouble((edittxt2.getText().toString()));
+                    double result= num1 / num2;
+                    textresult.setText(Double.toString(result));
+                }
+                else{
+                    Toast toast=Toast.makeText(MainActivity.this," Enter the numbers", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+
+            }
+
+        });
     }
-
-     */
-
 
 }
